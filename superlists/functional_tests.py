460b3aa77eaae862be4fb8aa12bc34cbfbc04d67
jaffer6 <aamir@jaffer.me.uk>
2016-09-14 16:15:53 +0100

    Refactored checking table text into a helper method

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -22,6 +22,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         # Runs after each test
         self.browser.quit()
+    
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
         
     def test_can_start_a_list_and_retrieve_it_later(self):        
         # Edith has heard about a cool new online todo-app.
@@ -43,12 +48,8 @@ class NewVisitorTest(unittest.TestCase):
         
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
-        inputbox.send_keys(Keys.ENTER)
-        
-        #time.sleep(10)
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        inputbox.send_keys(Keys.ENTER)        
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
         
         # There is still a text box inviting her to add another item
         # She enters "Use peacock feathers to make a fly
@@ -57,11 +58,8 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         
         # The page updates again, and now shows both items on her List
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
-        
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')                        
         
         # Edith wonders whether the site will remember her list. Then she
         # sees the site has generated a unique URL for her -- there is some
