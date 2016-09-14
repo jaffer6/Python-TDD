fc00bced35e247091c2ad77066e767cea1f49c57
jaffer6 <aamir@jaffer.me.uk>
2016-09-14 16:09:48 +0100

    Can add one todo only - refactoring

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -10,6 +10,7 @@ Created on 14 Sep 2016
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
+import time
 
 class NewVisitorTest(unittest.TestCase):
     
@@ -43,15 +44,24 @@ class NewVisitorTest(unittest.TestCase):
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
+        
+        #time.sleep(10)
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows),
-                        "New to-do item did not appear in the table")
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
         
         # There is still a text box inviting her to add another item
-        # She enters "Use peacock feathers to make a fly"
+        # She enters "Use peacock feathers to make a fly
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
         
         # The page updates again, and now shows both items on her List
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+        
         
         # Edith wonders whether the site will remember her list. Then she
         # sees the site has generated a unique URL for her -- there is some
