d453df4b4bef29592f6619b892c6bedc990f915a
jaffer6 <aamir@jaffer.me.uk>
2016-09-15 11:23:47 +0100

    new URL, view and template to display lists

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -41,7 +41,7 @@ class HomePageTest(TestCase):
         
         #Assert that we are redirected after a POST
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')      
+        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')      
         
     def test_home_page_only_saves_items_when_necessary(self):
         request = HttpRequest()
@@ -50,18 +50,21 @@ class HomePageTest(TestCase):
         
         self.assertEqual(Item.objects.count(), 0)
         
-    def test_home_page_displays_all_list_items(self):        
+        
+class ListViewTest(TestCase):
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
+    
+    def test_displays_all_items(self):        
         Item.objects.create(text='Itemey 1')
         Item.objects.create(text='Itemey 2')        
+                
+        response = self.client.get('/lists/the-only-list-in-the-world')
         
-        request = HttpRequest()
-        response = home_page(request)
-        
-        self.assertIn('Itemey 1', response.content.decode())
-        self.assertIn('Itemey 2', response.content.decode())
-        
-        
-        
+        self.assertContains(response, 'Itemey 1')
+        self.assertContains(response, 'Itemey 2')
+                    
         
 class ItemModelTest(TestCase):
     
