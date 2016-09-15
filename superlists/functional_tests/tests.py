8244f0dbdc64a6d07fcf2b19c27af2de321d675c
jaffer6 <aamir@jaffer.me.uk>
2016-09-15 10:49:04 +0100

    Coding functional test

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -70,15 +70,30 @@ class NewVisitorTest(LiveServerTestCase):
         
         ## We use a new browser session to make sure that no information of Edith's
         ## is coming through from cookies etc
+        self.browser.quit()
+        self.browser = webdriver.Chrome('C:\\chromedriver_win32\\chromedriver.exe')
         
         # Francis visits the home page. There is no sign of Edith's list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
         
         # Francis starts a new list by entering a new item. He is less interesting 
         # than Edith
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
         
         # Francis gets his own unique URL
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(edith_list_url, francis_list_url)
         
         # Again there is no sign of Edith's list
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn(page_text, 'Buy milk')
         
         # Satisfied they both go back to sleep
         self.fail("Finish the test!")
