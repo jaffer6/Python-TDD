e06c0c1b3f7b77624759b040913f79fda5d793e7
jaffer6 <aamir@jaffer.me.uk>
2016-09-16 14:42:03 +0100

    Adding unit tests to make functional tests pass

--- superlists/lists/tests/test_views.py
+++ superlists/lists/tests/test_views.py
@@ -4,6 +4,7 @@ from lists.views import home_page
 from django.http.request import HttpRequest
 from django.template.loader import render_to_string
 from lists.models import Item, List
+from django.utils.html import escape
 
 class HomePageTest(TestCase):
     
@@ -74,7 +75,23 @@ class NewListTest(TestCase):
         
         #Assert that we are redirected after a POST
         self.assertEqual(response.status_code, 302)
-        self.assertRedirects(response, '/lists/{}/'.format(new_list.id))     
+        self.assertRedirects(response, '/lists/{}/'.format(new_list.id)) 
+        
+    def test_validation_errors_are_sent_back_to_home_page_template(self):
+        response = self.client.post(
+            '/lists/new',
+            data={'item_text': ''}
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+        expected_error = escape("You can't have an empty list item")
+        #print(response.content.decode())
+        self.assertContains(response, expected_error)
+        
+    def test_invalid_list_items_are_not_saved(self):
+        self.client.post('/lists/new', data={'item_text': '' })
+        self.assertEqual(List.objects.count(), 0)
+        self.assertEqual(Item.objects.count(), 0)
         
 class NewItemTest(TestCase):
     
